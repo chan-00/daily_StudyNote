444c30fcd1ff46449242670f91df880dd2f8a847
chan-00 <a82316@naver.com>
2022-04-20 22:08:28 +0900

    220420_CppStudy.h 작성
    2022.04.20일자 C++ 공부 코드입니다.

---
+++ 220420_CppStudy.h
@@ -0,0 +1,30 @@
+﻿#ifndef __source__
+#define __source__
+
+#include<iostream>
+
+using std::string;
+
+class Car {
+private:
+	int speed;
+	int gear;
+	string color;
+	const int Max_Speed;
+	static int count;
+
+public:
+	Car();
+	Car(int s);
+	Car(int s, int g);
+	Car(int s, int g, string c);
+	Car(const Car& c);
+
+	void setSpeed(int s);
+	int getSpeed();
+	void print() const;
+	~Car();
+};
+
+
+#endif
